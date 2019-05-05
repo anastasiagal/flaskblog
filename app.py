6a6c3e726c6ff8ea2675bdcdfe19697d5b636bc4
Anastasya Galyamova <anastasya.galyamova@gmail.com>
2019-05-05 21:04:31 +0300

    added login form

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, flash, redirect
 import os
 from forms import LoginForm
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import models
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 # ... add more variables here as needed
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/')
@@ -35,3 +46,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
