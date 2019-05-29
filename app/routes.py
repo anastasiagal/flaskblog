84e13b7e5631efb52ef20ce4f74f29a1a8f40ad5
Anastasya Galyamova <anastasya.galyamova@gmail.com>
2019-05-29 20:04:11 +0300

    error processing

--- app/routes.py
+++ app/routes.py
@@ -27,16 +27,12 @@ def user(username):
 @app.route('/index')
 @login_required
 def index():
-    posts = [
-        {
+    posts = [{
             'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
+            'body': 'Beautiful day in Portland!'},
         {
             'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+            'body': 'The Avengers movie was so cool!'}]
     return render_template('index.html', title='Home', posts=posts)
 
 
@@ -82,7 +78,7 @@ def register():
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -92,5 +88,4 @@ def edit_profile():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title='Edit Profile',
-                           form=form)
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
